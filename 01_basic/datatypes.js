680df700a7850c47f4400a2ea11fb079aba48ffa
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-08-06 08:58:40 +0000

    datatypes

--- 01_basic/datatypes.js
+++ 01_basic/datatypes.js
@@ -1,1 +1,27 @@
 "use strict"; // treat all JS code as newer
+// alert(3+3) // we r using nodeJS not browser onnly browser shows popup
+
+let name = "Fatymah"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//Primitive data types
+//number 2 to power 53
+// bigint
+// string ""
+// bolean true/false
+// null --> standalone value
+// undefined --> not assigned
+// symbol -> unique
+
+//object
+
+console.log(typeof "Fatymah");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
+
