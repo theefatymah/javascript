24b81ba679d370fc2e43a03c977ed2422e44c9a9
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-08-06 11:50:41 +0000

    conversion

---
+++ 01_basic/conversion.js
@@ -0,0 +1,24 @@
+let score = undefined
+// const {score} = req.body
+console.log(typeof (score));
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" easily convert to 33
+// "33abc" convertion --> NaN
+// true --> 1, false -->0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let number = 33
+let string = string(number)
+console.log(typeof string);
+console.log(string);
+
+
+
