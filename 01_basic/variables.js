d3f13df2b054e0f6a55c323c8206135c37b6bd40
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-08-06 08:10:18 +0000

    Javascript

---
+++ 01_basic/variables.js
@@ -0,0 +1,20 @@
+console.log("javascript")
+const accountId = 14444444
+let accountEmail = "theefatymah@gmail.com"
+var accountPassword = "1234456"
+accountCity = "LHR"
+let accountState;
+
+// accountId = 2
+console.log(accountId);
+
+accountEmail = "8844noor@gmail.com"
+accountPassword = "456789"
+accountCity = "KHI"
+
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState])
+
+/*
+Prefer not to use var
+becoz of issue in lock scope and functional scope
+*/
