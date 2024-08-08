78718ece73a6aa5d1fe19db0ab2a421d13e8dd81
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-08-08 09:41:54 +0000

    datatypes

---
+++ 01_basic/datatypes-concept.js
@@ -0,0 +1,30 @@
+// primitive data types 
+// 7 types 
+// Number
+// String
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+// refernece datatypes
+// Array
+// Objects
+// Functions
+
+// Javascript --> dynamic language
+
+const score = 100;
+const scoreValue = 100.1;
+
+const isLoggedIn = true;
+const outsideTemp = null;
+
+let userEmail;
+const id = Symbol('123');
+const anotherId = Symbol('123')
+
+console.log( id === anotherId);
+
+const bigNumber = 889477858758711657076716078676n
