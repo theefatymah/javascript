c815dbbb11c83fd89271cc1b2b4e04f0102da2fc
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-08-08 08:49:53 +0000

    operations.js

--- 01_basic/operations.js
+++ 01_basic/operations.js
@@ -11,3 +11,19 @@ console.log(str3);
 console.log( "1" + 3);
 console.log( 6 + "4");
 console.log( "3" + 1 + 1);
+console.log( 1+ 3 + "3");
+
+
+console.log( ( 3 + 4 ) * 2 % 3 );
+
+console.log( true);
+console.log( + true);
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+
