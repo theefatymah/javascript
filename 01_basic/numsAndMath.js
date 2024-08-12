ae22955e45f6dc3d535a6be57747493b3e32a5c5
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-08-12 08:22:57 +0000

    math

--- 01_basic/numsAndMath.js
+++ 01_basic/numsAndMath.js
@@ -14,3 +14,32 @@ const hundreds = 10000000
 console.log(hundreds.toLocaleString('en-IN'));
 console.log(hundreds.toLocaleString());
 
+//-------------------Maths-----------------------
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(4.9));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+console.log(Math.max(10,34,67,35));
+console.log(Math.min(1,8,34,65));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * ( max - min + 1 ) + min ));
+
+
+
+
+
+
+
+
