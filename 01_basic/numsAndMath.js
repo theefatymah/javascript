98b985c1fc45e455e157f7639665e758a496a6ed
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-08-12 08:02:30 +0000

    numbers

---
+++ 01_basic/numsAndMath.js
@@ -0,0 +1,16 @@
+const score = 400
+console.log(score);
+
+const balance = new Number (100)
+console.log(balance);
+
+console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+
+const anotherNumber = 123.8967
+console.log(anotherNumber.toPrecision(3));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString());
+
