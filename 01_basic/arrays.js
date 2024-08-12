685309f94940ec2c8c66fa8b7bf8256ca08a1586
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-08-12 09:25:37 +0000

    arrays

---
+++ 01_basic/arrays.js
@@ -0,0 +1,22 @@
+const arr = [0, 1, 2, 3, 4, 5]
+const name = ['Fatymah','MahNoor']
+
+const myArr = new Array(0, 1, 2, 3, 4, 5)
+console.log(myArr[1]);
+
+arr.push(6)
+arr.push(8)
+arr.pop()
+
+
+arr.shift()
+arr.unshift(9)
+console.log(arr.includes(9));
+console.log(arr.indexOf(3));
+
+console.log(arr); 
+const newArray = arr.join()
+console.log(newArray);
+
+
+
