21ce74899a0efcddc861d06c9fe8cdb1036f14e4
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-08-27 09:17:39 +0000

    objects

--- 01_basic/objects.js
+++ 01_basic/objects.js
@@ -64,3 +64,43 @@ const obj2 = {3:"a", 4:"b"}
 // const obj3 = Object.assign({},obj1, obj2)
 const obj3 = {...obj1,...obj2}
 console.log(obj3);
+
+const users = [
+    {
+        id: 179847,
+        email : "h@gmail.com"
+    },
+    {
+        id: 179847,
+        email : "h@gmail.com"
+    },
+    {
+        id: 179847,
+        email : "h@gmail.com"
+    },
+    {
+        id: 179847,
+        email : "h@gmail.com"
+    },
+    {
+        id: 179847,
+        email : "h@gmail.com"
+    }
+    
+]
+
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+ 
+const course = {
+    courseName : "JS in Spanich",
+    price : "999"
+    courseInstructor : "Fatima"
+}
+
+const {courseInstructor} = course
+console.log(courseInstructor);
