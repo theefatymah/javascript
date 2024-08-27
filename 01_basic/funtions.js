77739d5c9bfd64b52a4366ef737cd91477a69a1e
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-08-27 10:15:47 +0000

    funtions

---
+++ 01_basic/funtions.js
@@ -0,0 +1,10 @@
+function sayMyName(){
+    console.log("F");
+    console.log("A");
+    console.log("T");
+    console.log("I");
+    console.log("M");
+    console.log("A");
+    console.log("h");
+}
+sayMyName()
