5f35557a77bf72a5be2add9c5f0cf84a3efa1abe
Mah Noor Fatima <126895895+theefatymah@users.noreply.github.com>
2024-09-02 07:31:11 +0000

    scope

--- 01_basic/scope.js
+++ 01_basic/scope.js
@@ -6,10 +6,10 @@ if(true){
     console.log( "INSIDE let ", a);
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 console.log('outside let ', a);
 
 function one(){
@@ -18,7 +18,33 @@ function one(){
         const website = "youtube"
         console.log(username);
     }
-    console.log(website);
+    // console.log(website);
     two()
 }
+
  one()
+
+
+
+ if (true){
+    const username = "Fatymah"
+    if(username === "Fatymah"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    //console.log(website);
+ }
+ //console.log(username);
+ 
+
+ function addone(num) {
+    return num + 1
+ }
+ addone(5)
+
+
+
+ const addTwo = funtion(num){
+    return num +2
+ }
+    
